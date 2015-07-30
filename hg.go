7a7cd6eaa0d1496a2cf3cec5a571bdb287c37e20
Matt Farina <matt@mattfarina.com>
2015-07-30 16:47:56 -0400

    Add support for Mercurial (Hg)

--- hg.go
+++ hg.go
@@ -1,91 +1,67 @@
 package vcs
 
-// import (
-// 	"fmt"
-// 	"os"
-// 	"os/exec"
-// 	"strings"
-// )
+import (
+	"os"
+	"os/exec"
+	"strings"
+)
 
-// // HgVCS implements the VCS interface for the Mercurial source control.
-// type HgVCS struct{}
+// NewHgRepo creates a new instance of HgRepo. The remote and local directories
+// need to be passed in.
+func NewHgRepo(remote, local string) *HgRepo {
+	r := &HgRepo{}
+	r.setRemote(remote)
+	r.setLocalPath(local)
 
-// // If you can help clean this up or improve it, please submit patches!
+	return r
+}
 
-// // Get implements the getting logic for hg.
-// func (h *HgVCS) Get(dep *Dependency) error {
-// 	dest := fmt.Sprintf("%s/src/%s", os.Getenv("GOPATH"), dep.Name)
-// 	//Info("Cloning %s into %s\n", dep.Repository, dest)
-// 	fmt.Print("[INFO] hg: ")
-// 	out, err := exec.Command("hg", "clone", "-U", dep.Repository, dest).CombinedOutput()
-// 	fmt.Print(string(out))
-// 	return err
-// }
+// HgRepo implements the Repo interface for the Mercurial source control.
+type HgRepo struct {
+	base
+}
 
-// // Update performs an hg update for an existing checkout.
-// func (h *HgVCS) Update(dep *Dependency) error {
-// 	dest := fmt.Sprintf("%s/src/%s", os.Getenv("GOPATH"), dep.Name)
+// Get is used to perform an initial clone of a repository.
+func (s *HgRepo) Get() error {
+	return s.run("hg", "clone", "-U", s.Remote(), s.LocalPath())
+}
 
-// 	if _, err := os.Stat(dest); err != nil {
-// 		// Assume a new package?
-// 		Info("Looks like %s is a new package. Cloning.", dep.Name)
-// 		return exec.Command("hg", "clone", "-U", dep.Repository, dest).Run()
-// 	}
+// Update performs a Mercurial pull to an existing checkout.
+func (s *HgRepo) Update() error {
+	return s.runFromDir("hg", "update")
+}
 
-// 	oldDir, err := os.Getwd()
-// 	if err != nil {
-// 		return err
-// 	}
-// 	os.Chdir(dest)
-// 	defer os.Chdir(oldDir)
+// UpdateVersion sets the version of a package currently checked out via Hg.
+func (s *HgRepo) UpdateVersion(version string) error {
+	return s.runFromDir("hg", "update", version)
+}
 
-// 	out, err := exec.Command("hg", "pull").CombinedOutput()
-// 	fmt.Print(string(out))
-// 	return err
-// }
+// Version retrieves the current version.
+func (s *HgRepo) Version() (string, error) {
 
-// // Version sets the version of a package currently checked out via Hg. For
-// // more detail see the SetReference function.
-// func (h *HgVCS) Version(dep *Dependency) error {
-// 	dest := fmt.Sprintf("%s/src/%s", os.Getenv("GOPATH"), dep.Name)
+	oldDir, err := os.Getwd()
+	if err != nil {
+		return "", err
+	}
+	os.Chdir(s.LocalPath())
+	defer os.Chdir(oldDir)
 
-// 	oldDir, err := os.Getwd()
-// 	if err != nil {
-// 		return err
-// 	}
-// 	os.Chdir(dest)
-// 	defer os.Chdir(oldDir)
+	out, err := exec.Command("hg", "identify").CombinedOutput()
+	if err != nil {
+		return "", err
+	}
 
-// 	//Info("Setting %s with 'hg checkout'\n", dep.Name)
+	parts := strings.SplitN(string(out), " ", 2)
+	sha := parts[0]
+	return strings.TrimSpace(sha), nil
+}
 
-// 	// Now get to the right reference.
-// 	if len(dep.Reference) > 0 {
-// 		if out, err := exec.Command("hg", "update", "-q", dep.Reference).CombinedOutput(); err != nil {
-// 			fmt.Println(string(out))
-// 			return err
-// 		}
-// 		Info("Set version to %s to %s\n", dep.Name, dep.Reference)
-// 	}
-// 	return nil
-// }
+// CheckLocal verifies the local location is a Git repo.
+func (s *HgRepo) CheckLocal() bool {
+	if _, err := os.Stat(s.LocalPath() + "/.hg"); err == nil {
+		return true
+	}
 
-// // LastCommit retrieves the current version.
-// func (h *HgVCS) LastCommit(dep *Dependency) (string, error) {
-// 	dest := fmt.Sprintf("%s/src/%s", os.Getenv("GOPATH"), dep.Name)
+	return false
 
-// 	oldDir, err := os.Getwd()
-// 	if err != nil {
-// 		return "", err
-// 	}
-// 	os.Chdir(dest)
-// 	defer os.Chdir(oldDir)
-
-// 	out, err := exec.Command("hg", "identify").CombinedOutput()
-// 	if err != nil {
-// 		return "", err
-// 	}
-// 	parts := strings.SplitN(string(out), " ", 2)
-
-// 	sha := parts[0]
-// 	return sha, nil
-// }
+}
---
+++ hg_test.go
@@ -0,0 +1,101 @@
+package vcs
+
+import (
+	"io/ioutil"
+	//"log"
+	"os"
+	"testing"
+)
+
+// Canary test to ensure HgRepo implements the Repo interface.
+var _ Repo = &HgRepo{}
+
+// To verify hg is working we perform intergration testing
+// with a known hg service.
+
+func TestHg(t *testing.T) {
+
+	tempDir, err := ioutil.TempDir("", "go-vcs-hg-tests")
+	if err != nil {
+		t.Error(err)
+	}
+	defer func() {
+		err = os.RemoveAll(tempDir)
+		if err != nil {
+			t.Error(err)
+		}
+	}()
+
+	repo := NewHgRepo("https://bitbucket.org/mattfarina/testhgrepo", tempDir+"/testhgrepo")
+
+	// Check the basic getters.
+	if repo.Remote() != "https://bitbucket.org/mattfarina/testhgrepo" {
+		t.Error("Remote not set properly")
+	}
+	if repo.LocalPath() != tempDir+"/testhgrepo" {
+		t.Error("Local disk location not set properly")
+	}
+
+	//Logger = log.New(os.Stdout, "", log.LstdFlags)
+
+	// Do an initial clone.
+	err = repo.Get()
+	if err != nil {
+		t.Errorf("Unable to clone Hg repo. Err was %s", err)
+	}
+
+	// Verify Hg repo is a Hg repo
+	if repo.CheckLocal() == false {
+		t.Error("Problem checking out repo or Hg CheckLocal is not working")
+	}
+
+	// Set the version using the short hash.
+	err = repo.UpdateVersion("a5494ba2177f")
+	if err != nil {
+		t.Errorf("Unable to update Hg repo version. Err was %s", err)
+	}
+
+	// Use Version to verify we are on the right version.
+	v, err := repo.Version()
+	if v != "a5494ba2177f" {
+		t.Error("Error checking checked out Hg version")
+	}
+	if err != nil {
+		t.Error(err)
+	}
+
+	// Perform an update.
+	err = repo.Update()
+	if err != nil {
+		t.Error(err)
+	}
+
+	v, err = repo.Version()
+	if v != "d680e82228d2" {
+		t.Error("Error checking checked out Hg version")
+	}
+	if err != nil {
+		t.Error(err)
+	}
+
+}
+
+func TestHgCheckLocal(t *testing.T) {
+	// Verify repo.CheckLocal fails for non-Hg directories.
+	// TestHg is already checking on a valid repo
+	tempDir, err := ioutil.TempDir("", "go-vcs-hg-tests")
+	if err != nil {
+		t.Error(err)
+	}
+	defer func() {
+		err = os.RemoveAll(tempDir)
+		if err != nil {
+			t.Error(err)
+		}
+	}()
+
+	repo := NewHgRepo("", tempDir)
+	if repo.CheckLocal() == true {
+		t.Error("Hg CheckLocal does not identify non-Hg location")
+	}
+}
