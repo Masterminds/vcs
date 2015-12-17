1fefd329b64b9d1c9008f735dc02814bdd40c813
Nithin Meppurathu <admin@nitmedia.com>
2015-12-16 21:11:19 -0500

    Update vcs_remote_lookup.go
    updated regex

--- vcs_remote_lookup.go
+++ vcs_remote_lookup.go
@@ -24,7 +24,7 @@ var vcsList = []*vcsInfo{
 	{
 		host:    "github.com",
 		vcs:     Git,
-		pattern: `^(github\.com/[A-Za-z0-9_.\-]+/[A-Za-z0-9_.\-]+)(/[A-Za-z0-9_.\-]+)*$`,
+		pattern: `^(github\.com[/|:][A-Za-z0-9_.\-]+/[A-Za-z0-9_.\-]+)(/[A-Za-z0-9_.\-]+)*$`,
 	},
 	{
 		host:     "bitbucket.org",
