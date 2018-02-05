4dafb2dc6205c7568aef36c351ca1628ea9165e7
Matthew Fisher <matt.fisher@microsoft.com>
2018-02-05 15:47:15 -0800

    remove speter.net from VCS remote tests
    speter.net/go/exp/math/dec/inf has moved to gopkg.in/inf.v0.

--- vcs_remote_lookup_test.go
+++ vcs_remote_lookup_test.go
@@ -34,7 +34,6 @@ func TestVCSLookup(t *testing.T) {
 		"https://example.com/foo/bar/baz.hg":                               {work: true, t: Hg},
 		"https://gopkg.in/tomb.v1":                                         {work: true, t: Git},
 		"https://golang.org/x/net":                                         {work: true, t: Git},
-		"https://speter.net/go/exp/math/dec/inf":                           {work: true, t: Git},
 		"https://git.openstack.org/foo/bar":                                {work: true, t: Git},
 		"git@github.com:Masterminds/vcs.git":                               {work: true, t: Git},
 		"git@example.com:foo.git":                                          {work: true, t: Git},
